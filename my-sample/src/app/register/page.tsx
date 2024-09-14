28cc6e5808714eda844b969f2a34f97d898c784b
ZL <ray040406@gmail.com>
2024-09-14 15:00:25 +1200

    styling and imports

--- my-sample/src/app/register/page.tsx
+++ my-sample/src/app/register/page.tsx
@@ -1,1 +1,11 @@
 import { Input } from "@/components/ui/input";
+import { Label } from "@radix-ui/react-dropdown-menu";
+import Link from "next/link";
+
+const Register = async () => {
+  return (
+    <div className="mt-10 max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white border border-[#121212] dark: bg-black">
+      <h2>Register page</h2>
+    </div>
+  );
+};
