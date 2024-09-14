f69d0256f1f9926f16c2c07f8fdedbc2ce2ff4b9
ZL <ray040406@gmail.com>
2024-09-14 15:03:06 +1200

    form set up

--- my-sample/src/app/register/page.tsx
+++ my-sample/src/app/register/page.tsx
@@ -6,6 +6,21 @@ const Register = async () => {
   return (
     <div className="mt-10 max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white border border-[#121212] dark: bg-black">
       <h2>Register page</h2>
+      <form>
+        <label>First Name</label>
+        <Input id="firstname" placeholder="Ray" type="text" name="firstname" />
+        <label>Last Name</label>
+        <Input id="lastname" placeholder="Ray" type="text" name="lastname" />
+        <Label>Email Address</Label>
+        <Input id="email" placeholder="email" type="text" name="email" />
+        <Label>Password</Label>
+        <Input
+          id="password"
+          placeholder="password"
+          type="text"
+          name="password"
+        />
+      </form>
     </div>
   );
 };
